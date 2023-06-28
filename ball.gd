306c607d9b7ee7efb26cc6a92729ea1e7e75a29b
Jackson Arnold <jackson.n.arnold@gmail.com>
2023-06-28 13:02:42 -0400

    Collision points included
    06/28/23, need to figure out how it actually works

--- ball.gd
+++ ball.gd
@@ -29,4 +29,26 @@ func _on_start_timer_timeout():
 	apply_central_impulse(serve_vector) 
 
 func _on_area_2d_area_entered(area):
-	print("Hit Paddle")
+	var current_velocity = self.linear_velocity
+	
+	apply_central_impulse(-current_velocity) # Cancel out current vector
+	
+	# Apply new vector with same magnitude as previous one, but a direction based on where paddle was hit
+	
+	# apply_central_impulse()
+
+func _on_area_2d_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+	var body_shape_owner_id = area.shape_find_owner(area_shape_index)
+	var body_shape_owner = area.shape_owner_get_owner(body_shape_owner_id)
+	var body_shape_2d = area.shape_owner_get_shape(body_shape_owner_id, 0)
+	var body_global_transform = body_shape_owner.global_transform
+	
+	var area_shape_owner_id = shape_find_owner(local_shape_index)
+	var area_shape_owner = shape_owner_get_owner(area_shape_owner_id)
+	var area_shape_2d = shape_owner_get_shape(area_shape_owner_id, 0)
+	var area_global_transform = area_shape_owner.global_transform
+	
+	var collision_points = area_shape_2d.collide_and_get_contacts(area_global_transform,
+									body_shape_2d,
+									body_global_transform)
+	print(collision_points)
