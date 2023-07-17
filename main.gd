3b5842ec5c36f169916a00b466f65bd2b0b90dae
Jackson Arnold <jackson.n.arnold@gmail.com>
2023-07-16 22:00:57 -0400

    Progress on generating bricks
    07/16/23

--- main.gd
+++ main.gd
@@ -1,12 +1,40 @@
 extends Node2D
 
 @export var simulation_tps = 60
+@export var brick_scene: PackedScene
+
+var remaining_bricks = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Engine.set_physics_ticks_per_second(simulation_tps)
-
+	start() # Temporary, HUD will call this later
+	
+# Setup and start game
+func start():
+	_generate_bricks()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
+
+func _on_ball_brick_destroyed():
+	remaining_bricks -= 1
+	
+	if(remaining_bricks <= 0):
+		_generate_bricks()
+	
+func _generate_bricks():
+	print("Generating Bricks")
+	
+	var bricks_added = 0
+	
+	for i in range(40,750,50):
+		var brick = brick_scene.instantiate()
+		add_child(brick)
+		
+		brick.position = Vector2(i, 110)
+		
+		bricks_added += 1
+		
+	remaining_bricks += bricks_added
